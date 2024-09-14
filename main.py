329349845737e7e12e267debfdd82c5bea144fe5
dimostant <dimos.2000@hotmail.com>
2024-09-14 23:08:20 +0300

    implemented answers

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import ChatGBT_db.devgpt_chats as chatgpt_db
-from so_api import get_api_question
+from so_api import get_api_question, get_api_answer #change to importing the entire file?
 from so_postgres import get_so_postgres_question
 
 #inputs
 question_id = 4 #can also be an array of ids
+user = 1
 path = 'ChatGBT_db/DevGPT/snapshot_20231012/20231012_235320_discussion_sharings.json'
 
 #figure out in what sequence to get DevGPT questions and StackOverflow api questions
 #check date of stackoverflow data to be before the release of ChatGPT
+#filter for what programming the question is about, maybe use the tags from stackoverflow and the answer too for both
 
 json_data = chatgpt_db.get_json_data(path) # optimize db interface? #just use json.dumps
-user_converstations = chatgpt_db.get_user_converstations(json_data, 1)
+user_converstations = chatgpt_db.get_user_converstations(json_data, user)
 
 #get data from DevGPT
 gpt_question = chatgpt_db.get_user_question(user_converstations, 0)
@@ -20,7 +22,7 @@ str_gpt_question = chatgpt_db.json_data_to_str(gpt_question)
 # gpt_answer = chatgpt_db.get_user_answer(user_converstations, 0) # gpt_code = chatgpt_db.get_user_code(user_converstations, 0)
 
 #get data from StackOverflow API
-so_api_question = get_api_question()
+so_api_question = get_api_question(question_id)
 chatgpt_db.print_json_data(so_api_question) #indent=2) # make json handler?
 
 str_so_api_question = chatgpt_db.json_data_to_str(so_api_question)
@@ -32,7 +34,7 @@ str_so_api_question = chatgpt_db.json_data_to_str(so_api_question)
 
 from compare import compare_questions  
 
-#compare different questions
+#compare different questions test case
 
 if compare_questions(str_so_api_question , str_gpt_question) >= 0.7:
     print("similar questions")
@@ -42,14 +44,32 @@ else:
 #compare similar or same questions
 
 if compare_questions(str_so_api_question, str_so_api_question) == 1:
-    print("same questions")
+    print("identical questions")
+    #ignore
 
 elif compare_questions(str_so_api_question, str_so_api_question) >= 0.7:
     print("similar questions")
 
-    
+    #compare answers codes code
+
+    #ramp of how much the code matches, 1 to 0.7/ 0.69 to 0.3/ 0.29 to 0 and categorize, where will this be stored?
 
     #if (answerA code for logokloph answerB)
     #print("logoklophhhhh")
 else:
     print("different questions")
+
+#ansers codes code
+so_api_answer = get_api_answer(question_id)
+chatgpt_db.print_json_data(so_api_answer)
+
+print("\n devgpt code \n")
+
+gpt_code = chatgpt_db.get_user_code(user_converstations, 0)
+chatgpt_db.print_json_data(gpt_code)
+
+
+
+# while gpt_question != None: 
+# get them 100 by 100 to optimize the process, maybe 50? do optimal call of the api  
+# while so_api_question ???:
--- so_api.py
+++ so_api.py
@@ -5,7 +5,7 @@ from stackapi import StackAPI
 SITE = StackAPI('stackoverflow')
 
 def get_api_question(list_of_ids):
-    if type(list_of_ids) == int: #ids must be an array, even of one element 
+    if type(list_of_ids) == int: #ids must be an array, even of one element #must be tested
         list_of_ids = [list_of_ids]
     api_question = SITE.fetch('questions/{ids}', ids = list_of_ids) 
     return api_question["items"][0]["title"]
@@ -14,4 +14,6 @@ def get_api_answer(list_of_ids):
     if type(list_of_ids) == int:
         list_of_ids = [list_of_ids]
     api_answer = SITE.fetch('answers/{ids}', ids = list_of_ids)
-    return api_answer["items"][0]["body"]
+
+    # return api_answer["items"][0]["body"]
+    return api_answer
