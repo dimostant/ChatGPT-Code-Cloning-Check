926a9c6b81a6e46211f0b203d39d85f65136f607
dimostant <dimos.2000@hotmail.com>
2024-09-26 14:48:42 +0300

    add commented tasks in main, converted SO_questionID to always be an array, removed check array logic

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import ChatGBT_db.devgpt_chats as chatgpt_db
 from compare_process import compare_process
 
 #inputs
-question_id = 79018992 #can also be an array of ids
+question_id = [79018992] #must be array
 user = 1
+#user_conversation
 path = 'ChatGBT_db/DevGPT/snapshot_20231012/20231012_235320_discussion_sharings.json'
 
 #get data from DevGPT
@@ -18,14 +19,19 @@ gpt_conversation = chatgpt_db.get_user_conversation(json_data, user, 0)
 #is there a use for the text of the answers?
 #include question and answers comments
 
+# method getAllquestions and filter bydate etc
 
-# while so_api_question ???:
+# method while so_api_question date < chatGPT release date ???:
+
+# method for i = 0 ; i <= id_lastpostbefore chatGPT release
+
 #traverse through all stackoverflow questions before [chatGPT releaseDate] by finding the last question id of that date or the date, any implications?
 #get them e.g. 100 by 100 to optimize the process, maybe 50? do optimal call of the api
 #question not empty
 #filter the question from the coding language tags, must be e.g. Python
 #extra code that confirms its python
 
+#for conversation in conversations:
 #somehow traverse through all DevGPT conversations
 #must be Python
 
@@ -35,3 +41,7 @@ compare_process(question_id, gpt_conversation)
 #identical case [questionId],[gpt_conversation]
 #different case [questionId],[gpt_conversation]
 #match     case [questionId],[gpt_conversation]
+
+#store the results in CSV or JSON and in what structure
+
+#HOW TO DO THE TESTIONG??
--- so_api.py
+++ so_api.py
@@ -5,15 +5,11 @@ from stackapi import StackAPI
 SITE = StackAPI('stackoverflow')
 
 def get_api_question(list_of_ids):
-    if type(list_of_ids) == int: #ids must be an array, even of one element #must be tested
-        list_of_ids = [list_of_ids]
     api_question = SITE.fetch('questions/{ids}', ids = list_of_ids)
 
     return api_question["items"][0]["title"]
 
 def get_api_answer(list_of_ids, order = 'desc', sort = 'activity'):
-    if type(list_of_ids) == int:
-        list_of_ids = [list_of_ids]
     api_questions = SITE.fetch('questions/{ids}/answers', ids = list_of_ids, filter = '!nNPvSNdWme') 
     api_question = api_questions["items"][0]["body"]
 
