25edc95873b11703f692de1d9fadb35e537804e1
dimostant <dimos.2000@hotmail.com>
2024-09-26 13:30:01 +0300

    change the compare_process to getting a questionId and a gpt conversation only as parameters

--- main.py
+++ main.py
@@ -1,44 +1,37 @@
 import ChatGBT_db.devgpt_chats as chatgpt_db
-from so_api import get_api_question #change to importing the entire file?
-from so_postgres import get_so_postgres_question
-from compare_process import compare_proccess
+from compare_process import compare_process
 
 #inputs
 question_id = 79018992 #can also be an array of ids
 user = 1
 path = 'ChatGBT_db/DevGPT/snapshot_20231012/20231012_235320_discussion_sharings.json'
 
+#get data from DevGPT
+json_data = chatgpt_db.get_json_data(path) # optimize db interface? #just use json.dumps
+
+#investigate if it is needed to include Extra DevGPT db data
+gpt_conversation = chatgpt_db.get_user_conversation(json_data, user, 0)
+
 #figure out in what sequence to get DevGPT questions and StackOverflow api questions
 #check date of stackoverflow data to be before the release of ChatGPT
 #filter for what programming the question is about, maybe use the tags from stackoverflow and the answer too for both
 #is there a use for the text of the answers?
-#include question and answerscomments
-
-#get data from DevGPT
-json_data = chatgpt_db.get_json_data(path) # optimize db interface? #just use json.dumps
-gpt_conversation = chatgpt_db.get_user_conversation(json_data, user, 0)
-
-gpt_question = chatgpt_db.get_conversation_question(gpt_conversation)
-str_gpt_question = chatgpt_db.json_data_to_str(gpt_question)
-print("DevGPT        question :", str_gpt_question)
+#include question and answers comments
 
 
-#get data from StackOverflow API
-so_api_question = get_api_question(question_id) # make json handler?
-str_so_api_question = chatgpt_db.json_data_to_str(so_api_question)
-print("StackOverflow question :", str_so_api_question)
+# while so_api_question ???:
+#traverse through all stackoverflow questions before [chatGPT releaseDate] by finding the last question id of that date or the date, any implications?
+#get them e.g. 100 by 100 to optimize the process, maybe 50? do optimal call of the api
+#question not empty
+#filter the question from the coding language tags, must be e.g. Python
+#extra code that confirms its python
 
-# #get data from StackOverflow Postgres Db
-# so_postgres_question = get_so_postgres_question()
-# print(so_postgres_question)
+#somehow traverse through all DevGPT conversations
+#must be Python
 
-#compare so_api_questiona argument, gpt_question
+#compare so_api_questions argument, gpt_question
+compare_process(question_id, gpt_conversation)
 
-compare_proccess(str_so_api_question, question_id, str_so_api_question)
-#THIS ONLY WORKS BECAUSE THE STRING IS IDENTICAL
-#identical case
-
-compare_proccess(str_so_api_question ,question_id, str_gpt_question)
-#different case
-
-#match case
+#identical case [questionId],[gpt_conversation]
+#different case [questionId],[gpt_conversation]
+#match     case [questionId],[gpt_conversation]
