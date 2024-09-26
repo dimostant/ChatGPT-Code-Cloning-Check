c1fe78eae8533582b978a0fa69f4536cdcb02f48
dimostant <dimos.2000@hotmail.com>
2024-09-26 05:25:38 +0300

    draft implimentation of compare_process.py

--- compare_process.py
+++ compare_process.py
@@ -1,11 +1,12 @@
 from compare import compare_questions
 from code_handling import extract_html_code, extract_dictionary_code
 import ChatGBT_db.devgpt_chats as chatgpt_db
+from so_api import get_api_answer
 
-
-def compare_proccess (so_api_question, gpt_question):
+#might change this to only passing the question_id
+def compare_proccess (so_api_question, so_question_id, gpt_conversation):
     #should the contexts of the questions even be checked before comparing code??
-    similarity = compare_questions(so_api_question, gpt_question)
+    similarity = compare_questions(so_api_question, gpt_conversation)
 
     if similarity == 1:
         print("identical questions\n")
@@ -14,41 +15,40 @@ def compare_proccess (so_api_question, gpt_question):
     elif 0.7 <= similarity < 1:
         print("similar questions, checking...\n")
 
-        # #ramp of how much the code matches, 1 to 0.7/ 0.69 to 0.3/ 0.29 to 0 and categorize, where will this be stored?
-        # #if (answerA code for logokloph answerB)
-        # #print("logoklophhhhh")
-        #
-        # #compare answers codes code
-        #
-        # print("\n so api code \n")
-        #
-        # # answers codes code
-        # so_api_answer_body = get_api_answer(question_id)
-        # # chatgpt_db.print_json_data(so_api_answer_body)
-        # so_api_answer_code = extract_html_code(so_api_answer_body)
-        # print(so_api_answer_code)
-        #
-        # # needs a function that cleans the string of irrelevant to the code text that is left over
-        # # insert code to abstract syntax tree (python code only!)
-        # # tree1 = ast.parse(extracted_so_api_code)
-        #
-        # print("\n devgpt code \n")
-        #
-        # gpt_answer_dictionary = chatgpt_db.get_user_code(user_conversations, 0)  # is this json or string?
-        # # chatgpt_db.print_json_data(gpt_answer_code_dictionary)
-        #
-        # # extract blocks of code to a string with \n and cleanup irrelavant information to the code
-        # gpt_answer_code = extract_dictionary_code(gpt_answer_dictionary)
-        # print(gpt_answer_code)
-        #
-        # # needs a function that cleans the string of irrelevant to the code text that is left over
-        #
-        # # insert code to abstract syntax tree (python code only!)
-        # # tree2 = ast.parse(extracted_gpt_answer_code )
-        #
-        # # while gpt_question != None:
-        # # get them 100 by 100 to optimize the process, maybe 50? do optimal call of the api
-        # # while so_api_question ???:
+        #ramp of how much the code matches, 1 to 0.7/ 0.69 to 0.3/ 0.29 to 0 and categorize, where will this be stored?
+        #if (answerA code for logokloph answerB)
+        #print("logoklophhhhh")
+
+        #compare answers codes code
+
+        print("\n so api code \n")
+
+        so_api_answer_body = get_api_answer(so_question_id)
+        # chatgpt_db.print_json_data(so_api_answer_body)
+        so_api_answer_code = extract_html_code(so_api_answer_body)
+        print(so_api_answer_code)
+
+        # needs a function that cleans the string of irrelevant to the code text that is left over
+        # insert code to abstract syntax tree (python code only!)
+        # tree1 = ast.parse(extracted_so_api_code)
+
+        print("\n devgpt code \n")
+
+        gpt_answer_dictionary = chatgpt_db.get_conversation_code(gpt_conversation)
+        # chatgpt_db.print_json_data(gpt_answer_code_dictionary)
+
+        # extract blocks of code to a string with \n and cleanup irrelavant information to the code
+        gpt_answer_code = extract_dictionary_code(gpt_answer_dictionary)
+        print(gpt_answer_code)
+
+        # needs a function that cleans the string of irrelevant to the code text that is left over
+
+        # insert code to abstract syntax tree (python code only!)
+        # tree2 = ast.parse(extracted_gpt_answer_code )
+
+        # while gpt_question != None:
+        # get them 100 by 100 to optimize the process, maybe 50? do optimal call of the api
+        # while so_api_question ???:
 
     else:
         print("different questions\n")
--- main.py
+++ main.py
@@ -12,13 +12,13 @@ path = 'ChatGBT_db/DevGPT/snapshot_20231012/20231012_235320_discussion_sharings.
 #check date of stackoverflow data to be before the release of ChatGPT
 #filter for what programming the question is about, maybe use the tags from stackoverflow and the answer too for both
 #is there a use for the text of the answers?
-#include comments
+#include question and answerscomments
 
 #get data from DevGPT
 json_data = chatgpt_db.get_json_data(path) # optimize db interface? #just use json.dumps
-conversation = chatgpt_db.get_user_conversation(json_data, user, 0)
+gpt_conversation = chatgpt_db.get_user_conversation(json_data, user, 0)
 
-gpt_question = chatgpt_db.get_conversation_question(conversation)
+gpt_question = chatgpt_db.get_conversation_question(gpt_conversation)
 str_gpt_question = chatgpt_db.json_data_to_str(gpt_question)
 print("DevGPT        question :", str_gpt_question)
 
@@ -34,11 +34,11 @@ print("StackOverflow question :", str_so_api_question)
 
 #compare so_api_questiona argument, gpt_question
 
-compare_proccess(str_so_api_question, str_so_api_question)
+compare_proccess(str_so_api_question, question_id, str_so_api_question)
 #THIS ONLY WORKS BECAUSE THE STRING IS IDENTICAL
 #identical case
 
-compare_proccess(str_so_api_question , str_gpt_question)
+compare_proccess(str_so_api_question ,question_id, str_gpt_question)
 #different case
 
 #match case
