a6500665816af9039491023b51823d2bbfb62c83
dimostant <dimos.2000@hotmail.com>
2024-11-17 21:47:17 +0200

    unfunction proccess to raw main code, cleanup cloning.py file

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import ChatGBT_db.devgpt_chats as chatgpt_db
-#from so_api import get_api_questions, get_api_answers  #change to importing the entire file?
+#import so_api
 
 from cloning import code_cloning_check
 from code_handling import extract_html_code, extract_html_text, extract_dictionary_code, remove_non_utf8_chars
 
-import ast
+#import ast
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
@@ -55,19 +55,19 @@ def compare_answers(so_question_id, gpt_conversation): #might change to answers
     if "".join(gpt_answer_clean_code.split()) != "": #removes all whitespaces
         # TODO: ADD SLEEP SO YOU DONT HIT THE THROTTLE AGAIN
 
-        # only for testing
+        # only for testing #modify for multiple answers
         answers_path = 'so_api_answers.json'
         so_api_answers_json = chatgpt_db.get_json_data(answers_path)
 
         # so_api_answers_json = get_api_answers(so_question_id) #put this out of the function?
         # TODO: take only the one with the most votes
 
-        for item in so_api_answers_json.get('items', []) or []:
+        for item in so_api_answers_json.get('items', []):
            if item.get("body"):
                 so_api_answer_body = item["body"]
                 so_api_answer_code = extract_html_code(so_api_answer_body)
                 so_api_answer_clean_code = remove_non_utf8_chars(so_api_answer_code)
-                # print("api code: ", so_api_answer_clean_code)
+                # print(" api code: ", so_api_answer_clean_code)
 
                 code_cloning_check(gpt_answer_clean_code, so_api_answer_clean_code)
                 # compare answer context?
@@ -76,35 +76,31 @@ def compare_answers(so_question_id, gpt_conversation): #might change to answers
                 # TODO: ramp of how much the code matches, 1 to 0.7/ 0.69 to 0.3/ 0.29 to 0 and categorize, where will this be stored?
                 # TODO: cloning Code comparison and result extraction, then store the results in CSV or JSON and in what structure
 
-
 # invent a function that can detect the kind of question, and get the appropriate answers e.t.c accordingly
 def compare_process (so_question_id, dev_gpt_data):
-    # only for testing
-    questions_path = 'so_api_questions.json'
+    questions_path = 'so_api_questions.json'  # only for testing
     so_api_questions_json = chatgpt_db.get_json_data(questions_path)
 
     # so_api_questions_json = get_api_questions(so_question_id)
-    #TODO: ADD SLEEP SO YOU DONT HIT THE THROTTLE AGAIN
 
-    for item in so_api_questions_json.get("items", []) or []:
+    for item in so_api_questions_json.get("items", []):
         # TODO: consider title, should the contexts of the questions even be checked before comparing code??
         if item.get("body") :
             so_api_question_body = item["body"] #improve? # for title?
-            str_so_api_question = extract_html_text(so_api_question_body) #test if this is right
+            str_so_api_question = extract_html_text(so_api_question_body)
             str_so_api_clean_question = remove_non_utf8_chars(str_so_api_question)
             # print("StackOverflow question :", str_so_api_clean_question)
 
-            counter = 0 #delete after testing
+            counter = 0 #delete after testing #used to choose number ( almost ) of gpt chats
 
-            #TODO: check for empty boxes everywhere
-            for source in dev_gpt_data.get("Sources", []) or []: #will not iterate if empty
-                for sharing_data in source.get("ChatgptSharing", []) or []:
-                    for gpt_conversation in sharing_data.get("Conversations", []) or []:
+            for source in dev_gpt_data.get("Sources", []):
+                for sharing_data in source.get("ChatgptSharing", []):
+                    for gpt_conversation in sharing_data.get("Conversations", []):
                         if gpt_conversation :
                             gpt_question = chatgpt_db.get_conversation_question(gpt_conversation)
-                            # TODO: check for empty question and others emtys like this
                             str_gpt_question = chatgpt_db.json_data_to_str(gpt_question)
-                            # TODO:check for chinese characters e.t.c and skip question #filter for python
+                            # TODO: check for empty question and others empty things like this ( i think {}) EVERYWHERE
+                            # TODO: check for chinese characters e.t.c and skip question # filter for python
                             # print("DevGPT        question :", str_gpt_question)
 
                             counter = counter + 1
@@ -114,8 +110,7 @@ def compare_process (so_question_id, dev_gpt_data):
 
                             similarity = 0.8
                             if similarity == 1:
-                                print("identical questions\n")
-                                # is that right?
+                                print("identical questions\n") #special case to check??
 
                             elif 0.7 <= similarity < 1:
                                 # print("similar questions, checking...\n")
@@ -124,28 +119,30 @@ def compare_process (so_question_id, dev_gpt_data):
                             else:
                                 print("different questions\n")
 
-                #  inner conv increase the counter e.g. 1 2 3, 3 seen out. 4 5, 5 out.
-                # "if" checks out the loop so need <=
+                #  inner conv increase the counter e.g. 1 2 3, 3 seen out. 4 5, 5 out. "if" checks out the loop so need >=
                 if counter >= 4: break
 
+# TODO: special way of choosing what questions to iterate in the json?
+# TODO: extra code that confirms its python?
 
-
-
-#inputs #must be arr #[79018992]
-question_id = [59325633]
-
-#get data from DevGPT
-dev_gpt_json = chatgpt_db.get_json_data('ChatGBT_db/DevGPT/snapshot_20231012/20231012_235320_discussion_sharings.json') # optimize db interface? #just use json.dumps
-
-compare_process(question_id, dev_gpt_json)
-
-# TODO: traverse through all stackoverflow questions before [chatGPT releaseDate] by finding the last question id of that date or the date, any implications?
-# TODO: get them e.g. 100 by 100 to optimize the process, maybe 50? do optimal call of the api
-# TODO: filter the question from the coding language tags, must be e.g. Python
-# TODO: extra code that confirms its python
-
+# TODO: HOW TO DO THE TESTING??
 # TODO: identical case [questionId],[gpt_conversation]
 # TODO: different case [questionId],[gpt_conversation]
 # TODO: match     case [questionId],[gpt_conversation]
 
-# TODO: HOW TO DO THE TESTING??
+
+#input must be arr
+question_id = [59325633]
+
+#get data from DevGPT
+dev_gpt_json = chatgpt_db.get_json_data('ChatGBT_db/DevGPT/snapshot_20231012/20231012_235320_discussion_sharings.json')
+
+#compare_process(question_id, dev_gpt_json)
+#id draws more requests
+
+#compare_process
+#file
+
+
+
+
--- so_api.py
+++ so_api.py
@@ -5,11 +5,13 @@ SITE = StackAPI('stackoverflow')
 
 def get_api_questions(list_of_ids):
     api_questions = SITE.fetch('questions/{ids}', ids = list_of_ids)
+    print("quota remaining : " + api_questions["quota_remaining"])
 
     return api_questions
 
 
 def get_api_answers(list_of_ids, order = 'desc', sort = 'activity'):
     api_questions = SITE.fetch('questions/{ids}/answers', ids = list_of_ids, filter = '!nNPvSNdWme')
+    print("quota remaining : ", api_questions["quota_remaining"])
 
     return api_questions
