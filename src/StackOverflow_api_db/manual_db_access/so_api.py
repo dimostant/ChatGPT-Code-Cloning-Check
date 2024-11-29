ee439607ded4a6b8ee40e571d5ba1a810679d93b
dimostant <dimos.2000@hotmail.com>
2024-11-29 02:31:14 +0200

    add maxpages parameters, sort code

--- src/StackOverflow_api_db/manual_db_access/so_api.py
+++ src/StackOverflow_api_db/manual_db_access/so_api.py
@@ -1,26 +1,18 @@
-from stackapi import StackAPI
 from datetime import datetime
-from src import constants
 from src.constants import SO_KEY
 
+from stackapi import StackAPI
 # https://api.stackexchange.com/docs/
 SITE = StackAPI('stackoverflow', key = SO_KEY)
 default_max_pages = SITE.max_pages
 key = SO_KEY
 
-def get_api_questions(list_of_ids):
+def get_api_questions(list_of_ids, max_pages):
+    SITE.max_pages = max_pages
     api_questions = SITE.fetch('questions/{ids}', ids = list_of_ids)
     print("quota remaining : ", api_questions["quota_remaining"])
 
     return api_questions
-
-
-def get_api_answers(list_of_ids, max_pages):
-    SITE.max_pages = max_pages
-    api_answers = SITE.fetch('questions/{ids}/answers', ids = list_of_ids, filter = '!nNPvSNdWme')
-    print("quota remaining : ", api_answers["quota_remaining"])
-
-    return api_answers
 
 
 def get_api_questions_advanced(max_pages):
@@ -31,3 +23,11 @@ def get_api_questions_advanced(max_pages):
     SITE.max_pages = max_pages
 
     return questions
+
+
+def get_api_answers(list_of_ids, max_pages):
+    SITE.max_pages = max_pages
+    api_answers = SITE.fetch('questions/{ids}/answers', ids = list_of_ids, filter = '!nNPvSNdWme')
+    print("quota remaining : ", api_answers["quota_remaining"])
+
+    return api_answers
