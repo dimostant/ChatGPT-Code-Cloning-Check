43af90b563eabd3c18cbfdccbbc12bc2939c3621
dimostant <dimos.2000@hotmail.com>
2024-12-03 04:19:08 +0200

    succesfully fetchi and write answers for 100 q_ids

--- src/StackOverflow_api_db/test.py
+++ src/StackOverflow_api_db/test.py
@@ -3,42 +3,37 @@ from collections import Counter
 from src.StackOverflow_api_db.manual_db_access.so_api import get_api_answers, get_api_questions
 
 #Imports array of json answers, under the corresponding question_id as key
-def craft_answers(): # question_ids):
-    with open('crafted_answers_arrays.json', 'r') as e: all_answers = json.load(e)
+def craft_answers(questions_max_pages): # question_ids):
+    with open('crafted_answers_arrays.json', 'r') as e:
+        try :
+            all_answers = json.load(e)
+        except :
+            print("file empty")
+            all_answers = {}
 
     if not "items" in all_answers.keys():
         all_answers = { "items": [] }
-        print("yay")
 
-    with open('questions_api_response.json', 'r') as e: all_questions = json.load(e) #for testing, remove
-
+    with open('questions_api_response.json', 'r') as e: all_questions = json.load(e)
     question_ids = list(set(item["question_id"] for item in all_questions["items"]))
-    #print(question_ids)
-
-    question_ids = question_ids[:3]
-    #print(question_ids)
+    #sort q_ids #implement while
+    question_ids = question_ids[:100]
 
     if question_ids:
-        # questions_max_pages = 25
-        # new_answers = get_api_answers(question_ids, questions_max_pages).get('items', []) # all question id's answers in a json
-        # print(new_answers)
-        # with open('test.json', 'w') as f: json.dump(new_answers, f, indent=4)
+        new_answers = get_api_answers(question_ids, questions_max_pages).get('items', []) # all question id's answers in a json
 
-        with open('test.json', 'r') as e:
-            new_answers = json.load(e)  # for testing, remove
+        # with open('test.json', 'w') as f: json.dump(new_answers, f, indent=4) # for testing, remove
+        # with open('test.json', 'r') as e: new_answers = json.load(e)  # for testing, remove
 
         # find unique q_ids in the response answers
         unique_question_ids = list(set(item[key] for item in new_answers for key in item.keys() if key == 'question_id'))
-        #print(unique_question_ids)
+
 
         # iterate through ids, check if exists, add if new
         for q_id in unique_question_ids:
             if not any(str(q_id) in item for item in all_answers["items"]):
-                print("in")
                 # extra check for incomplete answers amount
                 filtered_id_new_answers = list(item for item in new_answers if item['question_id'] == q_id)
-                print(filtered_id_new_answers)
-
                 filtered_new_answers_json_arr = {q_id: filtered_id_new_answers}  # order ids getting in?
 
                 all_answers["items"].append(filtered_new_answers_json_arr)
@@ -72,8 +67,7 @@ while has_more:
     print("has_more")
 
     # questions = get_api_questions_advanced() # TODO: set the next page
-    with open('questions_api_response.json', 'r') as e: all_questions = json.load(e)
-    #fortesting # TODO: separate python tagged but not written in python??
+    with open('questions_api_response.json', 'r') as e: all_questions = json.load(e) #fortesting
 
     # page limit is 25, fetch next pages until has_more false
     if not all_questions["has_more"]:
@@ -100,4 +94,4 @@ if 'all_questions' in vars():
 
 
 #fetch answers
-craft_answers() # question_ids) # implement loop inside
+craft_answers(25) # question_ids) # implement loop inside
