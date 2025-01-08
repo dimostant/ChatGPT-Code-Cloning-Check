39dc28070f2de1519c3e396f63af4e490df94438
dimostant <dimos.2000@hotmail.com>
2025-01-08 05:43:44 +0200

    more cleanup, answers_id_json return array and cut the loop when id is found for optimization.

--- src/main.py
+++ src/main.py
@@ -11,7 +11,6 @@ from cloning import code_cloning_check
 from code_handling import extract_html_code, extract_html_text, extract_dictionary_code, remove_non_utf8_chars
 
 
-# TODO: to test
 def compare_questions(api_question, gpt_question):
     # print removing the \n and replacing with " " for ease
     # print("\ncomparing questions :\n", api_question.replace("\n", " "), "\nand :\n", gpt_question.replace("\n", " "))
@@ -49,7 +48,7 @@ def compare_questions(api_question, gpt_question):
     # return cosine
     return 0.8
 
-def compare_answers(so_api_answers_json, gpt_conversation, xl): #might change to answers
+def compare_answers(so_api_answers_json, gpt_conversation): #might change to answers
     gpt_answer_dictionary = chatgpt_db.get_conversation_code(gpt_conversation)
     gpt_answer_code = extract_dictionary_code(gpt_answer_dictionary)
     gpt_answer_clean_code = remove_non_utf8_chars(gpt_answer_code)
@@ -59,11 +58,12 @@ def compare_answers(so_api_answers_json, gpt_conversation, xl): #might change to
     # TODO: check for empty DevGPT answer # test if this is done
     # remove all whitespaces
     if "".join(gpt_answer_clean_code.split()) != "":
-        for item in so_api_answers_json:
-            so_api_answer_id = item["answer_id"]
-            so_api_answer_body = item["body"]
+        for so_api_answer in so_api_answers_json:
+            so_api_answer_id = so_api_answer["answer_id"]
+            so_api_answer_body = so_api_answer["body"]
             so_api_answer_code = extract_html_code(so_api_answer_body)
             so_api_answer_clean_code = remove_non_utf8_chars(so_api_answer_code)
+            #TODO: the answer must contain code
             # print("api code: ", so_api_answer_clean_code)
 
             cloning_percentage = code_cloning_check(gpt_answer_clean_code, so_api_answer_clean_code) #TODO: hardcoded, change #TODO: rename?
@@ -97,30 +97,28 @@ def compare_answers(so_api_answers_json, gpt_conversation, xl): #might change to
             break
 
 def compare_process ():
-    # get questions
-    with open(os.path.join('StackOverflow_api_db', 'db', 'questions.json'), 'r') as q:
-        so_api_questions_json = json.load(q)
-
-    # get DevGPT conversations
+    # read all DevGPT conversations
     dev_gpt_json = chatgpt_db.get_json_data(
         'ChatGBT_db/DevGPT/snapshot_20231012/20231012_235320_discussion_sharings.json'
     )
+
+    # read all questions from db file
+    with open(os.path.join('StackOverflow_api_db', 'db', 'questions.json'), 'r') as q:
+        so_api_questions_json = json.load(q)
 
     # read all answers from db file
     with open(os.path.join('StackOverflow_api_db', 'db', 'answers.json'), 'r') as a:
         so_api_answers_json = json.load(a)
 
     counter1 = 0 # renove
-    # iterate through every question
-    for item in so_api_questions_json.get("items", []):
-        counter1 = counter1 + 1 # remove
-        # print(counter1) # remove
-
-        if item.get("body") :
-            counter = 0 # remove # used to choose number ( almost ) of gpt chats
-
-            so_api_question_id = item["question_id"]
-            so_api_question_body = item["body"] #improve? # for title?
+    # iterate through every so_api question
+    for so_api_question in so_api_questions_json.get("items", []):
+        if so_api_question.get("body") :
+            counter1 = counter1 + 1  # remove
+            # print(counter1)        # remove
+            counter = 0              # remove # used to choose number ( almost ) of gpt chats
+            so_api_question_id = so_api_question["question_id"]
+            so_api_question_body = so_api_question["body"] #TODO: improve? # for title?
             str_so_api_question = extract_html_text(so_api_question_body)
             str_so_api_clean_question = remove_non_utf8_chars(str_so_api_question)
             # print("StackOverflow question :", str_so_api_clean_question)
@@ -131,8 +129,7 @@ def compare_process ():
                     for gpt_conversation in sharing_data.get("Conversations", []):
                         if gpt_conversation :
                             counter = counter + 1 # remove
-                            # print(counter) # remove
-
+                            print(counter)        # remove
                             gpt_question = chatgpt_db.get_conversation_question(gpt_conversation)
                             str_gpt_question = chatgpt_db.json_data_to_str(gpt_question)
                             # TODO: check for empty question and others empty things like this ( i think {}) EVERYWHERE
@@ -148,39 +145,37 @@ def compare_process ():
                             ]
                             df.to_excel(os.path.join('..', 'results.xlsx'),  index=False)
 
+                            #TODO: could compare questions anyway and see posible similarities we didnt expect
+                            # TODO: way to get rid of try? (1)
                             if 0.7 <= similarity < 1:
                                 try:
                                     # TODO: does answer file need checking?
                                     # TODO: what is returned, if the id has no answers
-                                    # so_api_answers_json_of_id = (item for item in so_api_answers_json["items"] if list(item.keys())[0] == str(so_api_question_id))
-                                    so_api_answers_json_of_id = []
-                                    for item in so_api_answers_json["items"]:
-                                        if list(item.keys())[0] == str(so_api_question_id):
-                                            so_api_answers_json_of_id.append(item)
-
-                                    print("a List", so_api_answers_json_of_id[0])
-                                    print("next")
-                                    print("b List", so_api_answers_json_of_id[1])
-
+                                    so_api_id_answers_json = []
+                                    for so_api_id_answers in so_api_answers_json.get("items", []):
+                                        if list(so_api_id_answers.keys())[0] == str(so_api_question_id):
+                                            so_api_id_answers_json = so_api_id_answers[str(so_api_question_id)]
+                                            break
                                 except:
-                                    #TODO: should this throw error?
                                     continue
-
-                                #TODO: see if of_id answers is an issue
-                                if so_api_answers_json_of_id: # TODO:   and gpt_conversations[code (and answer?)]
-                                    compare_answers(so_api_answers_json_of_id, gpt_conversation, df)
+                                # TODO: should this throw error? (2)
+                                # TODO:  and gpt_conversations[code (and answer?)]
+                                if so_api_id_answers_json:
+                                    compare_answers(so_api_id_answers_json, gpt_conversation)
 
                 # inner conv increase the counter e.g. 1 2 3, 3 seen out. 4 5, 5 out. "if" checks out the loop so need >=
-                if counter >= 1: # remove
+                if counter >= 1:        # remove
                     print("Breaaaakkk") # remove
-                    break # remove
+                    break               # remove
 
-        if counter1 >= 1:
-            print("Breaaaakkk break")
-            break
+        if counter1 >= 1:               # remove
+            print("Breaaaakkk break")   # remove
+            break                       # remove
+
 
 # TODO: only compare with python code from DEVGPT, extra code that confirms its python code? where? ( at answer code extraction function? at data retrieval? after data retrieval? )
 
+# TODO: to test compare questions accuracy
 # TODO: identical case [questionId],[gpt_conversation]
 # TODO: different case [questionId],[gpt_conversation]
 # TODO: match     case [questionId],[gpt_conversation]
