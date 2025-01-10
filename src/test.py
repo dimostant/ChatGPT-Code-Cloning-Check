48570c907587d201275d044b38c0a31dd10bc6fd
dimostant <dimos.2000@hotmail.com>
2025-01-10 02:10:56 +0200

    progress on simian testing

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 import subprocess, tempfile
 
-code1 = b"""print("1")"""
-code2 = b"""print("1")"""
+code1 = b"print('1')"
+code2 = b"print('1')"
 
 code1_file = tempfile.TemporaryFile()
 code2_file = tempfile.TemporaryFile()
@@ -17,8 +17,15 @@ try:
 
     print(code2_file.read())
 
-    simian = subprocess.run(["java", "-jar", "../simian-academic/simian-4.0.0/simian-4.0.0.jar", code1_file, code2_file], check=True, text=True,
-                            capture_output=True)
+    # simian = subprocess.run(
+    #     ["java", "-jar", "/simian-academic/simian-4.0.0/simian-4.0.0.jar", code1_file.read(), code2_file.read()],
+    #     check=True, text=True, capture_output=True
+    # )
+    # print(simian.stdout)
+    simian = subprocess.run(
+        ["java", "-jar", "../simian-academic/simian-4.0.0/simian-4.0.0.jar", "../testcode1", "../testcode1"],
+        check=True, text=True, capture_output=True
+    )
     print(simian.stdout)
 
 finally:
