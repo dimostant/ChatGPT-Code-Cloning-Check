7c421987bff1bf55a8634bc522c2022bab876f81
dimostant <dimos.2000@hotmail.com>
2025-01-18 03:38:30 +0200

    simian working properly with the tempfiles

--- src/test.py
+++ src/test.py
@@ -1,36 +1,53 @@
-import subprocess, tempfile
+import os, subprocess, tempfile
 
-code1 = b"print('1')"
-code2 = b"print('1')"
+code1 = b"""import tempfile
+  
+f = tempfile.TemporaryFile() 
+  
+try: 
+  f.write(b'Welcome to geeksforgeeks') 
+  f.seek(0) 
+  data=f.read() 
+  print(data) 
+finally: 
+  f.close()"""
+code2 = b"""
 
-code1_file = tempfile.TemporaryFile()
-code2_file = tempfile.TemporaryFile()
 
-try:
+
+
+import tempfile 
+  
+f = tempfile.TemporaryFile() 
+  
+try: 
+  f.write(b'Welcome to geeksforgeeks') 
+  f.seek(0) 
+  data=f.read() 
+  print(data) 
+finally: 
+  f.close()"""
+
+with tempfile.NamedTemporaryFile(suffix=".txt", delete=False) as code1_file, \
+     tempfile.NamedTemporaryFile(suffix=".txt", delete=False) as code2_file:
+
     code1_file.write(code1)
-    code1_file.seek(0)
-
-    print(code1_file.read())
-
     code2_file.write(code2)
+
+    code1_file.seek(0)
     code2_file.seek(0)
 
-    print(code2_file.read())
-
-    # simian = subprocess.run(
-    #     ["java", "-jar", "/simian-academic/simian-4.0.0/simian-4.0.0.jar", code1_file.read(), code2_file.read()],
-    #     check=True, text=True, capture_output=True
-    # )
-    # print(simian.stdout)
-    simian = subprocess.run(
-        ["java", "-jar", "../simian-academic/simian-4.0.0/simian-4.0.0.jar", "./testcode1.txt", "./testcode2.txt"],
-        check=True, text=True, capture_output=True
-    )
-    print(simian.stdout)
-    print(simian)
-
-finally:
     code1_file.close()
     code2_file.close()
 
+try:
+    simian = subprocess.run(
+        # ["java", "-jar", "../simian-academic/simian-4.0.0/simian-4.0.0.jar", "../testcode1.txt", "../testcode2.txt"],
+        ["java", "-jar", "../simian-academic/simian-4.0.0/simian-4.0.0.jar", code1_file.name, code2_file.name],
+        text=True, capture_output=True # check=True
+    )
+    print(simian.stdout)
 
+finally:
+    os.remove(code1_file.name)
+    os.remove(code2_file.name)
